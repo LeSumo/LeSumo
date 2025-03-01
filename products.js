6928810eaa39abe5c829037ebb7ee1b1edaf17be
LeSumo <nibelo51@gmail.com>
2025-03-01 10:41:46 -0500

    Update products.js

--- products.js
+++ products.js
@@ -4,8 +4,8 @@ const productos = [
       id: 'libro',
       nombre: 'Enfoque de Cambio - En español, Versión Digital',
       descripcion: 'Un libro lleno de conocimientos valiosos para realizar cambios positivos y exitosos, tanto a nivel individual como societal. ¡No te lo pierdas!',
-      precio: '20.00',
-      imagen: 'images/libro.png',
+      precio: '14.00',
+      imagen: 'libro.png',
       enlaceDescarga: 'https://drive.google.com/uc?export=download&id=1bMTLqNAVz1rKDL0eJpDZ2voBqLmzLK-R',
       gratuito: false // Producto de pago
     },
@@ -13,7 +13,7 @@ const productos = [
       id: 'articulo-cientifico',
       nombre: 'Artículo Científico',
       descripcion: 'Artículo científico sobre temas de demografía.',
-      imagen: 'images/articulo.png',
+      imagen: 'articulo.png',
       enlaceDescarga: 'https://drive.google.com/uc?export=download&id=1WCUeJgAAGX-RPzSP88_n5f6qZn0e9gBz',
       gratuito: true // Producto gratuito
     },
@@ -23,7 +23,7 @@ const productos = [
         nombre: 'Article Scientifique',
         descripcion: 'Article scientifique sur des sujets de démographie en français.',
         precio: '1.00',
-        imagen: 'images/article.png',
+        imagen: 'article.png',
         enlaceDescarga: 'https://drive.google.com/uc?export=download&id=18spir0YGREQOXaC5ZWb3XKECpEwcrlw3',
         gratuito: false // Producto de pago
       },
@@ -32,8 +32,8 @@ const productos = [
        id: 'Livre',
       nombre: 'Approche du changement-En français,Version numérique',
       descripcion: 'Un livre rempli de connaissances précieuses pour réaliser des changements positifs et réussis, tant au niveau individuel que sociétal. Ne le manquez pas',
-      precio: '20.00',
-      imagen: 'images/livre.png',
+      precio: '14.00',
+      imagen: 'livre.png',
       enlaceDescarga: 'https://drive.google.com/uc?export=download&id=1tLlV3C5bS33YAaxAz5kSR1NP5HJHtqo4',
       gratuito: false // Producto de pago
     }
