d0bb7e393099004f9abb68d8da7ce9a410c1b0a2
LeSumo <nibelo51@gmail.com>
2025-03-02 00:39:04 -0500

    Update products.js

--- products.js
+++ products.js
@@ -6,7 +6,7 @@ const productos = [
       descripcion: 'Un libro lleno de conocimientos valiosos para realizar cambios positivos y exitosos, tanto a nivel individual como societal. ¡No te lo pierdas!',
       precio: '14.00',
       imagen: 'libro.png',
-      enlaceDescarga: 'https://drive.google.com/uc?export=download&id=1bMTLqNAVz1rKDL0eJpDZ2voBqLmzLK-R',
+      enlaceDescarga: 'https://www.dropbox.com/scl/fi/stdgm6sbhkuo837nmtj82/Enfoque-de-cambio-Berthony-Pierrin.pdf?rlkey=9k7sihc1edxgquxveq3ugg5wo&st=kohon7s8&dl=1',
       gratuito: false // Producto de pago
     },
     {
@@ -14,7 +14,7 @@ const productos = [
       nombre: 'Artículo Científico',
       descripcion: 'Artículo científico sobre temas de demografía.',
       imagen: 'articulo.png',
-      enlaceDescarga: 'https://drive.google.com/uc?export=download&id=1WCUeJgAAGX-RPzSP88_n5f6qZn0e9gBz',
+      enlaceDescarga: 'https://www.dropbox.com/scl/fi/fige84uzy7u9a6oup2tow/Articulo-de-Berthony-PIERRIN.pdf?rlkey=7r5333cf1jx3ogpsdxxqtl2pe&st=iabht0nq&dl=1',
       gratuito: true // Producto gratuito
     },
 
@@ -24,7 +24,7 @@ const productos = [
         descripcion: 'Article scientifique sur des sujets de démographie en français.',
         precio: '1.00',
         imagen: 'article.png',
-        enlaceDescarga: 'https://drive.google.com/uc?export=download&id=18spir0YGREQOXaC5ZWb3XKECpEwcrlw3',
+        enlaceDescarga: 'https://www.dropbox.com/scl/fi/z4z5umwl3v5ybygkk61rq/Publication_Article_BerthonyPIERRIN.pdf?rlkey=ijgk4nvq2unnnddmho8ygt2qn&st=5d8mztb7&dl=1',
         gratuito: false // Producto de pago
       },
     
@@ -34,7 +34,7 @@ const productos = [
       descripcion: 'Un livre rempli de connaissances précieuses pour réaliser des changements positifs et réussis, tant au niveau individuel que sociétal. Ne le manquez pas',
       precio: '14.00',
       imagen: 'livre.png',
-      enlaceDescarga: 'https://drive.google.com/uc?export=download&id=1tLlV3C5bS33YAaxAz5kSR1NP5HJHtqo4',
+      enlaceDescarga: 'https://www.dropbox.com/scl/fi/lgggi8rfgz2vb6czjahho/Approche-du-changement-Berthony-PIERRIN.pdf?rlkey=chsnme5596duebqs1ok1aex6e&st=mb1zricl&dl=1',
       gratuito: false // Producto de pago
     }
   ];
