4743afa9fb15445242549766fc981c7c38ea5ebd
LeSumo <nibelo51@gmail.com>
2025-03-03 10:15:12 -0500

    Update products.js

--- products.js
+++ products.js
@@ -1,23 +1,6 @@
 // products.js
 const productos = [
     {
-      id: 'libro',
-      nombre: 'Enfoque de Cambio - En español, Versión Digital',
-      descripcion: 'Un libro lleno de conocimientos valiosos para realizar cambios positivos y exitosos, tanto a nivel individual como societal. ¡No te lo pierdas!',
-      precio: '14.00',
-      imagen: 'libro.png',
-      enlaceDescarga: 'https://www.dropbox.com/scl/fi/stdgm6sbhkuo837nmtj82/Enfoque-de-cambio-Berthony-Pierrin.pdf?rlkey=9k7sihc1edxgquxveq3ugg5wo&st=kohon7s8&dl=1',
-      gratuito: false // Producto de pago
-    },
-    {
-      id: 'articulo-cientifico',
-      nombre: 'Artículo Científico',
-      descripcion: 'Artículo científico sobre temas de demografía.',
-      imagen: 'articulo.png',
-      enlaceDescarga: 'https://www.dropbox.com/scl/fi/fige84uzy7u9a6oup2tow/Articulo-de-Berthony-PIERRIN.pdf?rlkey=7r5333cf1jx3ogpsdxxqtl2pe&st=iabht0nq&dl=1',
-      gratuito: true // Producto gratuito
-    },
-    {
         id: 'article scientifique',
         nombre: 'Article Scientifique',
         descripcion: 'Article scientifique sur des sujets de démographie en français.',
@@ -34,4 +17,21 @@ const productos = [
       enlaceDescarga: 'https://www.dropbox.com/scl/fi/lgggi8rfgz2vb6czjahho/Approche-du-changement-Berthony-PIERRIN.pdf?rlkey=chsnme5596duebqs1ok1aex6e&st=mb1zricl&dl=1',
       gratuito: false // Producto de pago
     },
+    {
+      id: 'libro',
+      nombre: 'Enfoque de Cambio - En español, Versión Digital',
+      descripcion: 'Un libro lleno de conocimientos valiosos para realizar cambios positivos y exitosos, tanto a nivel individual como societal. ¡No te lo pierdas!',
+      precio: '14.00',
+      imagen: 'libro.png',
+      enlaceDescarga: 'https://www.dropbox.com/scl/fi/stdgm6sbhkuo837nmtj82/Enfoque-de-cambio-Berthony-Pierrin.pdf?rlkey=9k7sihc1edxgquxveq3ugg5wo&st=kohon7s8&dl=1',
+      gratuito: false // Producto de pago
+    },
+    {
+      id: 'articulo-cientifico',
+      nombre: 'Artículo Científico',
+      descripcion: 'Artículo científico sobre temas de demografía.',
+      imagen: 'articulo.png',
+      enlaceDescarga: 'https://www.dropbox.com/scl/fi/fige84uzy7u9a6oup2tow/Articulo-de-Berthony-PIERRIN.pdf?rlkey=7r5333cf1jx3ogpsdxxqtl2pe&st=iabht0nq&dl=1',
+      gratuito: true // Producto gratuito
+    },  
   ];
