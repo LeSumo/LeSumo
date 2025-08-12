f587642a5017109eecf1dcc47bc52c3c841b8898
LeSumo <nibelo51@gmail.com>
2025-08-12 12:37:02 -0500

    Update products.js

--- products.js
+++ products.js
@@ -3,7 +3,7 @@ const productos = [
     {
         id: 'article scientifique',
         nombre: 'Article Scientifique',
-        descripcion: 'Article scientifique sur des sujets de démographie en français.',
+        descripcion: 'Article scientifique sur des sujets de démographie en français. Autor:Pierrin, Berthony',
         imagen: 'article.png',
         enlaceDescarga: 'https://www.dropbox.com/scl/fi/z4z5umwl3v5ybygkk61rq/Publication_Article_BerthonyPIERRIN.pdf?rlkey=ijgk4nvq2unnnddmho8ygt2qn&st=5d8mztb7&dl=1',
         gratuito: true // Producto de pago
@@ -11,7 +11,7 @@ const productos = [
     {
        id: 'Livre',
       nombre: 'Approche du changement-En français,Version numérique',
-      descripcion: 'Un livre rempli de connaissances précieuses pour réaliser des changements positifs et réussis, tant au niveau individuel que sociétal. Ne le manquez pas',
+      descripcion: 'Un livre rempli de connaissances précieuses pour réaliser des changements positifs et réussis, tant au niveau individuel que sociétal. Ne le manquez pas. Auteur:Pierrin, Berthony',
       precio: '14.00',
       imagen: 'livre.png',
       enlaceDescarga: 'https://www.dropbox.com/scl/fi/lgggi8rfgz2vb6czjahho/Approche-du-changement-Berthony-PIERRIN.pdf?rlkey=chsnme5596duebqs1ok1aex6e&st=mb1zricl&dl=1',
@@ -20,7 +20,7 @@ const productos = [
     {
       id: 'libro',
       nombre: 'Enfoque de Cambio - En español, Versión Digital',
-      descripcion: 'Un libro lleno de conocimientos valiosos para realizar cambios positivos y exitosos, tanto a nivel individual como societal. ¡No te lo pierdas! ISBN 978-607-29-7131-8',
+      descripcion: 'Un libro lleno de conocimientos valiosos para realizar cambios positivos y exitosos, tanto a nivel individual como societal. ¡No te lo pierdas! ISBN 978-607-29-7131-8. Autor:Pierrin, Berthony',
       precio: '14.00',
       imagen: 'Enfoque_cambio.png',
       enlaceDescarga: 'https://www.dropbox.com/scl/fi/stdgm6sbhkuo837nmtj82/Enfoque-de-cambio-Berthony-Pierrin.pdf?rlkey=9k7sihc1edxgquxveq3ugg5wo&st=kohon7s8&dl=1',
@@ -29,7 +29,7 @@ const productos = [
     {
       id: 'articulo-cientifico',
       nombre: 'Artículo Científico',
-      descripcion: 'Artículo científico sobre temas de demografía.',
+      descripcion: 'Artículo científico sobre temas de demografía. Autor:Pierrin, Berthony',
       imagen: 'articulo.png',
       enlaceDescarga: 'https://www.dropbox.com/scl/fi/fige84uzy7u9a6oup2tow/Articulo-de-Berthony-PIERRIN.pdf?rlkey=7r5333cf1jx3ogpsdxxqtl2pe&st=iabht0nq&dl=1',
       gratuito: true // Producto gratuito
@@ -38,7 +38,7 @@ const productos = [
     {
       id: 'Livre',
      nombre: '🎧 Enfoque de Cambio – Audiolibro en Español',
-     descripcion: 'Escucha este poderoso audiolibro y descubre los fundamentos clave para lograr cambios positivos y duraderos en tu vida y en la sociedad. Ideal para quienes prefieren aprender mientras se mueven, trabajan o descansan. ¡Haz del cambio un hábito diario!',
+     descripcion: 'Escucha este poderoso audiolibro y descubre los fundamentos clave para lograr cambios positivos y duraderos en tu vida y en la sociedad. Ideal para quienes prefieren aprender mientras se mueven, trabajan o descansan. ¡Haz del cambio un hábito diario! Autor:Pierrin, Berthony',
      precio: '14.00',
      imagen: 'Audio_Libro.png',
      enlaceDescarga: 'https://www.dropbox.com/scl/fi/8hzrxuuoyod2erp8hfoyl/Audio_Enfoque-de-Cambio_MP3.mp3?rlkey=bi6s8rcca7ghu4gxf3l821m8g&st=ztzuh3um&dl=1',
@@ -48,11 +48,12 @@ const productos = [
    {
     id: 'Livre',
    nombre: '🎧 Approche du Changement – Livre audio en français',
-   descripcion: 'Écoutez ce puissant livre audio et découvrez les principes clés pour réussir des changements positifs et durables, tant sur le plan personnel que sociétal. Parfait pour apprendre en marchant, en travaillant ou en se détendant. Faites du changement une habitude au quotidien !',
+   descripcion: 'Écoutez ce puissant livre audio et découvrez les principes clés pour réussir des changements positifs et durables, tant sur le plan personnel que sociétal. Parfait pour apprendre en marchant, en travaillant ou en se détendant. Faites du changement une habitude au quotidien ! Auteur:Pierrin, Berthony',
    precio: '14.00',
    imagen: 'Audio_Livre.png',
    enlaceDescarga: 'https://www.dropbox.com/scl/fi/gmni45k9tzaj7i9ti1x65/Audio_Approche_du_Changement.mp3?rlkey=qctf9eqgncnmj3vl9xyd33p60&st=xz3z43qn&dl=1',
    gratuito: false // Producto de pago
  }
   ];
+
 
